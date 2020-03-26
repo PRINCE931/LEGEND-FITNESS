c346133d8d875cc5285c15f61573b0a21d668423
PRINCE RANA <RANAPRINCE983@GMAIL.COM>
2020-03-26 15:37:03 +0530

    Initial Commit

---
+++ LEGEND FITNESS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LEGEND FITNESS
+//
+//  Created by PRINCE RANA on 3/26/20.
+//  Copyright © 2020 PRINCE RANA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
